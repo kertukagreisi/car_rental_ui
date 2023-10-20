e36716caeaf7e81f44f64569b584c73dc8620c67
Greisi Kertuka <greisikertuka@gmail.com>
2023-10-20 22:18:25 +0200

    Fis users and other pages to get correct responses

--- lib/page/user/user_page.dart
+++ lib/page/user/user_page.dart
@@ -1,16 +1,9 @@
-import 'package:car_rental_ui/generated_code/lib/api.dart';
-import 'package:car_rental_ui/navigation/nav_extensions.dart';
-import 'package:car_rental_ui/navigation/nav_route.dart';
 import 'package:car_rental_ui/resources/app_colors.dart';
 import 'package:car_rental_ui/resources/dimens.dart';
 import 'package:car_rental_ui/shared/locator.dart';
 import 'package:car_rental_ui/shared/mvvm/view_model_widgets.dart';
-import 'package:car_rental_ui/widgets/car_carousel_widget.dart';
-import 'package:car_rental_ui/widgets/text_container.dart';
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
-import '../../widgets/car_card_widget.dart';
 import 'user_view_model.dart';
 
 class UserPage extends ViewModelWidget<UserViewModel> {
@@ -18,8 +11,6 @@ class UserPage extends ViewModelWidget<UserViewModel> {
 
   @override
   Widget builder(BuildContext context, UserViewModel viewModel, Widget? child) {
-    final TextEditingController searchController = TextEditingController();
-
     return Container(
       color: Colors.white,
       padding: const EdgeInsets.all(16.0),
@@ -27,95 +18,36 @@ class UserPage extends ViewModelWidget<UserViewModel> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Padding(
-              padding: EdgeInsets.only(bottom: 10.0),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 12.0),
               child: Row(
                 children: [
-                  Expanded(
-                    child: Text('Our top rated cars!', style: Dimens.mediumHeadTextStyle),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 20.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text('Username', style: Dimens.mediumHeadTextStyle),
+                        Text(viewModel.user.username ?? '', style: Dimens.mediumTextStyle.copyWith(color: AppColors.darkGray)),
+                      ],
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text('Role', style: Dimens.mediumHeadTextStyle),
+                      Text(viewModel.user.role?.value ?? '', style: Dimens.mediumTextStyle.copyWith(color: AppColors.darkGray)),
+                    ],
                   ),
                 ],
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 10.0),
-              child: CarouselSlider(
-                options: CarouselOptions(
-                  height: 250,
-                  viewportFraction: 300 / MediaQuery.of(context).size.width,
-                  enlargeCenterPage: true,
-                  autoPlay: true,
-                ),
-                items: viewModel.user.map((car) {
-                  return Builder(
-                    builder: (BuildContext context) {
-                      return SizedBox(
-                        width: 250,
-                        child: CarCarouselWidget(
-                            car: car,
-                            onCarCardItemClick: (id) {
-                              context.goNamedRoute(NavRoute.booking, queryParams: {'id': '$id'}, extra: car);
-                            }),
-                      );
-                    },
-                  );
-                }).toList(),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 12.0),
-              child: TextFormField(
-                controller: searchController,
-                decoration: const InputDecoration(
-                  hintText: 'Search',
-                  border: InputBorder.none,
-                  prefixIcon: Icon(Icons.search, color: AppColors.darkBlue),
-                ),
-                onFieldSubmitted: (value) async {
-                  await viewModel.onSearch(value);
-                },
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 10.0),
+            const Padding(
+              padding: EdgeInsets.only(bottom: 12.0),
               child: Wrap(
-                spacing: 8.0,
-                runSpacing: 12.0,
-                children: [Brand.BMW, Brand.VOLKSWAGEN, Brand.MERCEDES_BENZ, Brand.AUDI]
-                    .map(
-                      (brand) => Padding(
-                        padding: const EdgeInsets.only(right: 4.0),
-                        child: TextButton(
-                          style: TextButton.styleFrom(
-                            minimumSize: Size.zero,
-                            padding: EdgeInsets.zero,
-                            tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                          ),
-                          onPressed: () {
-                            viewModel.addBrandFilter = brand;
-                          },
-                          child: TextContainer(
-                              text: brand.value,
-                              textColor: Colors.black,
-                              backgroundColor: viewModel.brandFiltersValues.contains(brand) ? AppColors.lightPurple : AppColors.lightGray,
-                              fontWeight: FontWeight.w300),
-                        ),
-                      ),
-                    )
-                    .toList(growable: false),
+                direction: Axis.vertical,
+                children: [],
               ),
-            ),
-            Wrap(
-              alignment: WrapAlignment.start,
-              spacing: 20.0,
-              runSpacing: 16.0,
-              children: [
-                ...viewModel.displayedCars.map((car) => CarCardWidget(
-                    car: car,
-                    onCarCardItemClick: (id) {
-                      context.goNamedRoute(NavRoute.booking, queryParams: {'id': '$id'}, extra: car);
-                    })),
-              ],
             ),
           ],
         ),
--- lib/page/user/user_view_model.dart
+++ lib/page/user/user_view_model.dart
@@ -15,29 +15,6 @@ class UserViewModel extends ViewModel {
   }
 
   Future<void> loadData() async {
-    user = await CarRentalApi.userEndpointApi.userLoginPost(LoginRequest('greisi', '123'))!;
-    displayedCars = user;
-  }
-
-  set addBrandFilter(Brand brand) {
-    if (brandFiltersValues.length == 1 && brandFiltersValues.first == brand) {
-      brandFiltersValues = [];
-      displayedCars = user;
-    } else {
-      if (brandFiltersValues.contains(brand)) {
-        brandFiltersValues.remove(brand);
-      } else {
-        brandFiltersValues.add(brand);
-      }
-      displayedCars =
-          user.where((car) => brandFiltersValues.contains(Brand.values.firstWhere((brand) => brand == car.brand))).toList(growable: false);
-    }
-    notifyListeners();
-  }
-
-  Future<void> onSearch(String value) async {
-    displayedCars =
-        user.where((car) => car.model!.contains(value.toUpperCase()) || car.brand!.value.contains(value.toUpperCase())).toList(growable: false);
-    notifyListeners();
+    user = (await CarRentalApi.userEndpointApi.userLoginPost(loginRequest: LoginRequest(username: 'greisi', password: '123')))!;
   }
 }
